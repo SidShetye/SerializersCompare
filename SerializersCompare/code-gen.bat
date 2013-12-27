a5bd15fc98efc17001497ad2c63b33967471bad1
Sid Shetye <sid@crypteron.com>
2013-12-27 01:27:44 -0800

    renamed lib folder

--- SerializersCompare/code-gen.bat
+++ SerializersCompare/code-gen.bat
@@ -1,7 +1,9 @@
 @echo off
 echo Running Avro code generation tool ...
-..\lib\avro-csharp-1.7.5\codegen\Release\avrogen.exe -s Serializers\Avro\InheritedEntity.avsc ..
+..\lib\avro-1.7.5\codegen\Release\avrogen.exe -s Serializers\Avro\InheritedEntityAvro.avsc ..
+..\lib\avro-1.7.5\codegen\Release\avrogen.exe -s Serializers\Avro\SimpleEntityAvro.avsc ..
 move Serializers\InheritedEntityAvro.cs Serializers\Avro
+move Serializers\SimpleEntityAvro.cs Serializers\Avro
 
 echo Running Thrift code generation tool ...
 ..\lib\thrift-0.9.1\thrift-0.9.1.exe -out Serializers\Thrift\ -gen csharp Serializers\Thrift\InheritedEntityThrift.thrift
